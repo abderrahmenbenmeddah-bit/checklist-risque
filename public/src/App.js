002078684043d5f2a2b8680b49a3aeedab203ba7
abderrahmenbenmeddah-bit <abderrahmen.benmeddah@gmail.com>
2026-02-18 11:00:27 +0100

    Ajout du fichier App.js

---
+++ public/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CheckList from "./pages/CheckList";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Check-list Visite de Risque</h1>
+      <CheckList />
+    </div>
+  );
+}
+
+export default App;
